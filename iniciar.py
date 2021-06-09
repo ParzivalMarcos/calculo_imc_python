323e9482f65516f74f22386b8e2998d4a262f305
Marcos Lima Marinho <marcolim977@gmail.com>
2021-06-09 12:41:07 -0300

    Adicionando funções para atualização de informações na tela e limpeza dos dados inseridos

--- iniciar.py
+++ iniciar.py
@@ -26,34 +26,46 @@ while True:
     if event == sg.WIN_CLOSED:
         break
 
+    # -- Escreve o valor do IMC na tela
+    def write_imc():
+        window['status_imc'].update(f"IMC: {objimc.imc}", visible=True)
+
+    # -- Limpa os campos de preenchimento
+    def clear_values():
+        window['altura'].update("")
+        window['kilo'].update("")
+
+
     if event == 'Calcular':
         if isnumber(value['altura']) and isnumber(value['kilo']):
             objimc = CalculaIMC(float(value['kilo']), float(value['altura']))
 
-            def write_imc():
-                window['status_imc'].update(f"IMC: {objimc.imc}", visible=True)
-
             if objimc.status_peso == 0:
                 window['status_peso'].update("Abaixo do peso", visible=True, text_color="yellow")
                 write_imc()
+                clear_values()
             elif objimc.status_peso == 1:
                 window['status_peso'].update("Peso normal", visible=True, text_color="green")
                 write_imc()
+                clear_values()
             elif objimc.status_peso == 2:
                 window['status_peso'].update("Sobrepeso", visible=True, text_color="red")
                 write_imc()
+                clear_values()
             elif objimc.status_peso == 3:
                 window['status_peso'].update("Obesidade Grau 1", visible=True, text_color="red")
                 write_imc()
+                clear_values()
             elif objimc.status_peso == 4:
                 window['status_peso'].update("Obesidade Grau 2", visible=True, text_color="red")
                 write_imc()
+                clear_values()
             elif objimc.status_peso == 5:
                 window['status_peso'].update("Obesidade Grau 3", visible=True, text_color="red")
                 write_imc()
-                
+                clear_values()
+
         else:
             layouts.pop_up_value_error()
-            window['altura'].update("")
-            window['kilo'].update("")
+            clear_values()
 window.close()
